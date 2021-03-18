d9010de836c31ea732322af10717d5e2eb74abaa
Andrzej <a.glowacki98@gmail.com>
2021-03-18 23:32:05 +0100

    added : comments in Main class

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,30 @@ import java.io.ObjectOutputStream;
 public class Main {
     public static void main(String[] args) {
         Main klasaMain = new Main();
+
+        //nazwa pliku do którego zapiszemy bazę produktow
         String nazwaPliku = "produkt.save";
 
+        //tworzę produkty do zapisu na bazie
         Produkt maslo = new Produkt("maslo", Produkt.UNIT_KG, true);
         Produkt maslo1 = new Produkt("maslo1", Produkt.UNIT_KG, true);
         Produkt maslo2 = new Produkt("maslo2", Produkt.UNIT_KG, true);
         Produkt mleko = new Produkt("mleko", Produkt.UNIT_L, false);
 
-        //druga wersja
+        //tworzę listę bazy i dodaję produkty
         ListaProduktow listaProduktow2 = new ListaProduktow(1);
         listaProduktow2.addToList(maslo);
         listaProduktow2.addToList(maslo1);
         listaProduktow2.addToList(maslo2);
         listaProduktow2.addToList(mleko);
 
+        //zapisuję listę produktów do pliku (to jest nasza baza produktow)
         klasaMain.WriteObjectToFile(listaProduktow2, nazwaPliku);
+
+        //załadowanie bazy z pliku
         ListaProduktow nowaLista2 = (ListaProduktow) klasaMain.ReadObjectFromFile(nazwaPliku);
+
+        //wypisanie zawartości bazy produktów
         nowaLista2.printOut();
     }
 
