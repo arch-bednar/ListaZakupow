7c41b5b2839a6a746e147010e11d41f5a1ec36b7
Andrzej <a.glowacki98@gmail.com>
2021-03-18 23:32:42 +0100

    new class added: ProduktNaLiscie

---
+++ src/ProduktNaLiscie.java
@@ -0,0 +1,9 @@
+public class ProduktNaLiscie {
+    private Produkt produkt;
+    private double ilosc;
+    private boolean wybrane;
+
+    ProduktNaLiscie(Produkt produkt, double ilosc, boolean wybrane){
+
+    }
+}
