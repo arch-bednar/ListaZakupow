ae712d441f834fad4735ee979d53434da50deaaf
Andrzej <a.glowacki98@gmail.com>
2021-03-27 17:18:22 +0100

    wyprowadziłem dodawanie produktów

--- app/src/main/java/com/example/myapp/DodajProdukt.java
+++ app/src/main/java/com/example/myapp/DodajProdukt.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 
 public class DodajProdukt extends AppCompatActivity {
     Button back, save;
+    Spinner dropdown;
+    String[] items;
+    EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +37,24 @@ public class DodajProdukt extends AppCompatActivity {
                 saveNewProduct();
             }
         });
+
+        dropdown = findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        items = new String[]{"KG", "SZT", "L"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        text = (EditText) findViewById(R.id.nazwaProduktu);
+
     }
 
     private void saveNewProduct() {
-        System.out.println("Zapisano nowy produkt!");
+        System.out.println("#####################################################\n              DODANO PRODUKT!\n     ");
+        System.out.println(text.getText()+"  "+items[(int) dropdown.getSelectedItemId()] );
+
         backToProducts();
     }
 
